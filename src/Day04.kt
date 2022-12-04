c9eb16957cdd6db134278a3438fc45892c9f2ff0
Antonio J Sanchez-Padial <antonio@spadial.com>
2022-12-04 07:34:34 +0100

    Day four

---
+++ src/Day04.kt
@@ -0,0 +1,58 @@
+fun main() {
+
+    fun getRange(rangeString: String): IntRange {
+        val values = rangeString.split("-")
+        val min = values[0].toInt()
+        val max = values[1].toInt()
+
+        return min..max
+    }
+
+
+   fun part1(input: List<String>): Int {
+
+        var score = 0
+
+       for (lines in input) {
+           val pair = lines.split(",")
+
+           val range1 = getRange(pair[0])
+           val range2 = getRange(pair[1])
+
+           if (((range2.contains(range1.first) && range2.contains(range1.last))
+                       || (range1.contains(range2.first) && range1.contains((range2.last))))) {
+               score++
+           }
+       }
+
+
+        return score
+    }
+
+    fun part2(input: List<String>): Int {
+        var score = 0
+
+        for (lines in input) {
+            val pair = lines.split(",")
+
+            val range1 = getRange(pair[0])
+            val range2 = getRange(pair[1])
+
+            if (((range2.contains(range1.first) || range2.contains(range1.last))
+                        || (range1.contains(range2.first) || range1.contains((range2.last))))) {
+                score++
+            }
+        }
+
+        return score
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
